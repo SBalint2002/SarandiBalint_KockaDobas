da7fcd3e10fe9c83657f0d85fef51cf3cc863045
Sárándi <sarandi.balint@gmail.com>
2022-10-13 21:43:31 +0200

    Change and Roll anim

--- app/src/main/java/hu/petrik/kockadobas/MainActivity.java
+++ app/src/main/java/hu/petrik/kockadobas/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,8 +15,11 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    private int szamlalo = 0;
     private ImageView elsoKocka;
     private ImageView masodikKocka;
     private Button egyKocka;
@@ -22,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView eredmeny;
     private Button dobas;
     private static boolean egykockae = false;
-    private int[] kepek = {R.drawable.kocka1,R.drawable.kocka2,R.drawable.kocka3, R.drawable.kocka4, R.drawable.kocka5, R.drawable.kocka6};
+    private int[] kepek = {R.drawable.kocka1, R.drawable.kocka2, R.drawable.kocka3, R.drawable.kocka4, R.drawable.kocka5, R.drawable.kocka6};
     private Button reset;
     private AlertDialog.Builder resetGomb;
 
@@ -31,19 +36,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
         egyKocka.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                masodikKocka.setVisibility(View.GONE);
-                egykockae = true;
+                if (!egykockae){
+                    elsoKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out_fade_out));
+                    masodikKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out_fade_out));
+                    elsoKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in_fade_in));
+                    masodikKocka.setVisibility(View.GONE);
+                    egykockae = true;
+                }
             }
         });
 
         kettoKocka.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                masodikKocka.setVisibility(View.VISIBLE);
-                egykockae = false;
+                if (egykockae){
+                    elsoKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out_fade_out));
+                    elsoKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in_fade_in));
+                    masodikKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in_fade_in));
+                    masodikKocka.setVisibility(View.VISIBLE);
+                    egykockae = false;
+                }
             }
         });
 
@@ -56,7 +72,9 @@ public class MainActivity extends AppCompatActivity {
                 resetGomb.setCancelable(false);
                 resetGomb.setNegativeButton("Nem", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i){}});
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
                 resetGomb.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -71,25 +89,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Random r = new Random();
-                int elso = r.nextInt(6);
-                elsoKocka.setImageResource(kepek[elso]);
-                if (!egykockae){
+                if (!egykockae) {
+                    int elso = r.nextInt(6);
                     int masodik = r.nextInt(6);
+                    //Első kocka eltűnés-megváltoztatás-előhozás
+                    elsoKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out_fade_out));
+                    elsoKocka.setImageResource(kepek[elso]);
+                    elsoKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in_fade_in));
+
+                    //Második kocka eltűnés-megváltoztatás-előhozás
+                    masodikKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out_fade_out));
                     masodikKocka.setImageResource(kepek[masodik]);
-                    eredmeny.append((elso+1) + " + " + (masodik+1) + "\n");
-                    Toast.makeText(MainActivity.this, (elso+1) + " + " + (masodik+1), Toast.LENGTH_SHORT).show();
-                }else{
-                    eredmeny.append((elso+1) + "\n");
-                    Toast.makeText(MainActivity.this, ""+(elso+1), Toast.LENGTH_SHORT).show();
+                    masodikKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in_fade_in));
+
+                    //Eredmény
+                    eredmeny.append((elso + 1) + " + " + (masodik + 1) + "\n");
+                    Toast.makeText(MainActivity.this, (elso + 1) + " + " + (masodik + 1), Toast.LENGTH_SHORT).show();
+                } else {
+                    int elso = r.nextInt(6);
+                    //Második kocka eltüntetés - Első kocka eltűnés-megváltoztatás-előhozás
+                    masodikKocka.setVisibility(View.GONE);
+                    elsoKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out_fade_out));
+                    elsoKocka.setImageResource(kepek[elso]);
+                    elsoKocka.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in_fade_in));
+
+                    //Eredmény
+                    eredmeny.append((elso + 1) + "\n");
+                    Toast.makeText(MainActivity.this, "" + (elso + 1), Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
 
-
     }
 
-    private void init(){
+    private void init() {
         elsoKocka = findViewById(R.id.elsoKocka);
         masodikKocka = findViewById(R.id.masodikKocka);
         egyKocka = findViewById(R.id.egyKocka);
