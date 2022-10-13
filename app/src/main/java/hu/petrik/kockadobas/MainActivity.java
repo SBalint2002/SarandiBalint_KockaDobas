ed247677c9ab223b4bf321ced9705de03e4f673d
Sárándi <sarandi.balint@gmail.com>
2022-10-13 14:44:00 +0200

    Add Pictures & init

--- app/src/main/java/hu/petrik/kockadobas/MainActivity.java
+++ app/src/main/java/hu/petrik/kockadobas/MainActivity.java
@@ -1,14 +1,27 @@
 package hu.petrik.kockadobas;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView elsoKocka;
+    private ImageView masodikKocka;
+    private Button egyKocka;
+    private Button kettoKocka;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        elsoKocka = findViewById(R.id.elsoKocka);
+        masodikKocka = findViewById(R.id.masodikKocka);
+        egyKocka = findViewById(R.id.egyKocka);
+        kettoKocka = findViewById(R.id.kettoKocka);
     }
 }
