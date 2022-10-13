0b5d6d634ce6ee6017edd2c5d20804d584b5b248
Sárándi <sarandi.balint@gmail.com>
2022-10-13 15:47:49 +0200

    Ãoran idaig jutottam

--- app/src/main/java/hu/petrik/kockadobas/MainActivity.java
+++ app/src/main/java/hu/petrik/kockadobas/MainActivity.java
@@ -1,6 +1,9 @@
 package hu.petrik.kockadobas;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private Button dobas;
     private static boolean egykockae = false;
     private int[] kepek = {R.drawable.kocka1,R.drawable.kocka2,R.drawable.kocka3, R.drawable.kocka4, R.drawable.kocka5, R.drawable.kocka6};
+    private Button reset;
+    private AlertDialog.Builder resetGomb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /*reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetGomb = new AlertDialog.Builder(MainActivity.this);
+                resetGomb.setTitle("Helyes megfejtés!");
+                resetGomb.setMessage("Szeretnél még egyet játszani?");
+                resetGomb.setCancelable(false);
+                resetGomb.setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                resetGomb.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                resetGomb.create().show();
+            }
+        });*/
+
         dobas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
 
     private void init(){
@@ -69,5 +99,6 @@ public class MainActivity extends AppCompatActivity {
         kettoKocka = findViewById(R.id.kettoKocka);
         eredmeny = findViewById(R.id.eredmeny);
         dobas = findViewById(R.id.dobas);
+        reset = findViewById(R.id.reset);
     }
 }
