23c8d1c42c75d2efc3b7f517b82ed1c6804b192c
Sárándi <sarandi.balint@gmail.com>
2022-10-13 14:54:03 +0200

    1 Kocka, 2 Kocka button OnClickListener

--- app/src/main/java/hu/petrik/kockadobas/MainActivity.java
+++ app/src/main/java/hu/petrik/kockadobas/MainActivity.java
@@ -2,20 +2,38 @@ package hu.petrik.kockadobas;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView elsoKocka;
     private ImageView masodikKocka;
     private Button egyKocka;
     private Button kettoKocka;
+    private TextView eredmeny;
+    private Button dobas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        egyKocka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                masodikKocka.setVisibility(View.GONE);
+            }
+        });
+
+        kettoKocka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                masodikKocka.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 
     private void init(){
@@ -23,5 +41,7 @@ public class MainActivity extends AppCompatActivity {
         masodikKocka = findViewById(R.id.masodikKocka);
         egyKocka = findViewById(R.id.egyKocka);
         kettoKocka = findViewById(R.id.kettoKocka);
+        eredmeny = findViewById(R.id.eredmeny);
+        dobas = findViewById(R.id.dobas);
     }
 }
